4485c7cf908c5679f52a0d4a9f092de717019351
Mrhu999 <1018526@qq.com>
2026-01-04 05:14:32 +0800

    Update modal.js

--- modal.js
+++ modal.js
@@ -7,3 +7,12 @@ function openModal() {
 function closeModal() {
   modal.style.display = "none";
 }
+const modal = document.getElementById("modal");
+
+function openModal() {
+  modal.style.display = "flex";  // 显示上传弹窗
+}
+
+function closeModal() {
+  modal.style.display = "none"; // 关闭上传弹窗
+}

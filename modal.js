313d74bd7b6e0e42883964849fc9b89c3373be05
Mrhu999 <1018526@qq.com>
2026-01-04 05:29:22 +0800

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,9 +1,9 @@
-const modal = document.getElementById("modal");
+const modal = document.getElementById("modal");  // 获取 modal 元素
 
 function openModal() {
-  modal.style.display = "flex";  // 显示上传弹窗
+  modal.style.display = "flex"; // 显示 modal
 }
 
 function closeModal() {
-  modal.style.display = "none"; // 关闭上传弹窗
+  modal.style.display = "none"; // 关闭 modal
 }
